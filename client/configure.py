1a3e273103231ed5c4a9ee6618f4e568426c0f2e
Il Nuovo <mirkobru2007@gmail.com>
2025-04-03 18:37:45 +0200

    finalized extractor

--- client/configure.py
+++ client/configure.py
@@ -18,7 +18,22 @@ class configurator:
         with open(name,"w") as nwFile:
             json.dump(Self.DEFAULT_CONFIGURATION, nwFile)
 
+    def save_conf (Self, data, path : str):
+        name = path.strip()
+        with open(name,"w") as nwFile:
+            json.dump(data, nwFile)
+    
+    def extract_conf (Self, path : str):
+        name = path.strip()
+        res = None
+        with open(name,"r") as nwFile:
+            res = json.load(nwFile)
+        return res
+
 #example:
 #
 config = configurator()
-config.create_conf("client/conf.json")
+#config.create_conf("./conf.json")
+extr = config.extract_conf("./conf.json")
+extr["track"] = False
+config.save_conf(extr, "./conf.json")
